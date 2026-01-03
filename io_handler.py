18f2009afd165fa52269f0ba940ebcfaa5de5ce5
Icaro Rodrigues <icarord13@gmail.com>
2026-01-03 14:09:00 -0300

    Fix the 'save to json' function.

--- io_handler.py
+++ io_handler.py
@@ -4,7 +4,7 @@ def save_sales(file_name, list_sales):
     try:
         with open(file_name, 'w', encoding='utf-8') as file:
             sales = list_sales
-            json.dump(sales, file_name, ensure_ascci=False, indent=4)
+            json.dump(sales, file, ensure_ascci=False, indent=4)
         return f'Dados salvo com sucesso em {file_name}'
     except Exception as e:
         return f'Erro ao salvar o arquivo\n \
